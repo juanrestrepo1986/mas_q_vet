8e1ffe0f04f73953b267acf846fc481fdb13a554
juanrestrepo1986 <92964382+juanrestrepo1986@users.noreply.github.com>
2022-02-06 18:06:06 -0500

    login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Estás conectado!') }}
                 </div>
             </div>
         </div>
